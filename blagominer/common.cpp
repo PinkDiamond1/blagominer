129eeb2dfa73f5f713c2a1ccd2d4ca8b60054a83
quetzalcoatl <quetzalcoatl.pl@gmail.com>
2019-06-27 17:43:27 +0200

    updated version

--- blagominer/common.cpp
+++ blagominer/common.cpp
@@ -1,7 +1,7 @@
 #include "common.h"
 
 const unsigned int versionMajor = 2;
-const unsigned int versionMinor = 300000;
+const unsigned int versionMinor = 300001;
 const unsigned int versionRevision = 0;
 
 // blago version
