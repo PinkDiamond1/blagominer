b7432d7ca88c9a731c8ead85cd440a8caf62da06
quetzalcoatl <quetzalcoatl.pl@gmail.com>
2019-07-03 12:33:32 +0200

    added reference code for calculating deadlines, added some tests for calculating deadlines, some test cases taken from felixbrucker for verifying math, some test cases from my miner logs (DLs reported by the pool as invalid for unknown reasons)

---
+++ blagominer/reference/BurstMath.h
@@ -0,0 +1,10 @@
+#pragma once
+
+#include "../common.h"
+
+namespace BurstMath
+{
+	uint32_t calculate_scoop(uint64_t height, uint8_t *gensig);
+
+	uint64_t calcdeadline(uint64_t account_nr, uint64_t nonce_nr, uint32_t scoop_nr, uint64_t currentBaseTarget, uint8_t currentSignature[32]);
+}
