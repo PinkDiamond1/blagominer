a4955a21bffc73cf7162f4553e96904c8cd7fb43
quetzalcoatl <quetzalcoatl.pl@gmail.com>
2019-07-04 09:17:23 +0200

    added more R&D test cases, added notes and some new cases to inspect

---
+++ blagominer-tests/deadlines.todo.cpp
@@ -0,0 +1,144 @@
+// BT5:'crash overnight BT5 2019-06-30 01-38-20'
+//03:03:49.795 Boom: {"error":{"message":"DL verification failed, your plot file might be corrupt. AccountId: 7955621360090688183 | Nonce: 16500290252717922814","code":10}}
+//03:03:49.261 SSEF: found deadline=1231588 scoop=1811 nonce=16500290252717922814 for account: 7955621360090688183 file: 7955621360090688183_16500290252717898080_30512
+//03:03:49.261 SIG: 1231588 <= 415eb3baba7000e164a58ba44cf6eb5d9bb29207d8f4e979589fd42b2f0692e4
+//03:03:49.261 SCP: 1231588 <= 995f5de7f1920e08d3c30ddf8b118629a7ca5a85ff40cf3bf0707a284e3794d6 f0ea409f769d45641dfcb11cdde802ee7d7fed4b3ed3a335fcb18a2c24bd6a6e
+//02:51:27.323 SSEF: found deadline=1231588 scoop=1811 nonce=16500290252717922814 for account: 7955621360090688183 file: 7955621360090688183_16500290252717898080_30512
+//02:51:27.323 SIG: 1231588 <= 415eb3baba7000e164a58ba44cf6eb5d9bb29207d8f4e979589fd42b2f0692e4
+//02:51:27.323 SCP: 1231588 <= 995f5de7f1920e08d3c30ddf8b118629a7ca5a85ff40cf3bf0707a284e3794d6 f0ea409f769d45641dfcb11cdde802ee7d7fed4b3ed3a335fcb18a2c24bd6a6e
+//02:51:17.970 *! GMI Boom: Received new mining info: {"height":684,"baseTarget":123538,"generationSignature":"415eb3baba7000e164a58ba44cf6eb5d9bb29207d8f4e979589fd42b2f0692e4","targetDeadline":31536000}
+//02:51:27.747 [ 7955621360090688183|Boom      |Sender] DL confirmed :     1231588      14d 06:06:28
+
+//03:06:24.250 Boom: {"error":{"message":"DL verification failed, your plot file might be corrupt. AccountId: 7955621360090688183 | Nonce: 16500290252708090652","code":10}}
+//03:03:57.973 *! GMI Boom: Received new mining info: {"height":686,"baseTarget":133652,"generationSignature":"1db3915f3b7516888973044bdce61e5489f96e8f86295440641dbf793e40b01b","targetDeadline":31536000}
+//03:04:17.545 SSEF: found deadline=20056547 scoop=2196 nonce=16737781616097994948 for account: 7955621360090688183 file: 7955621360090688183_16737781616097979640_19072
+//03:04:17.545 SIG: 20056547 <= 1db3915f3b7516888973044bdce61e5489f96e8f86295440641dbf793e40b01b
+//03:04:17.545 SCP: 20056547 <= 6db94c6e53b022203deca8b638a0ce41579c9358682c4db32151a299bfe0db77 d2bf7218881d8d7ff5ab579c5d767f0d00de14d343479b669f7491861b1a98aa
+//03:04:21.675 SSEF: found deadline=19818361 scoop=2196 nonce=16500290252713036834 for account: 7955621360090688183 file: 7955621360090688183_16500290252713016160_30512
+//03:04:21.675 SIG: 19818361 <= 1db3915f3b7516888973044bdce61e5489f96e8f86295440641dbf793e40b01b
+//03:04:21.675 SCP: 19818361 <= a5f481be99f4d58046d73f6d83bb21561f997689cae83a3e3a1307b4d13101bf 99d84428f1a4cf3f4b280791c73179337955af5d39ce5a31a77193b69cc2a60b
+//03:04:21.798 SSEF: found deadline=17198179 scoop=2196 nonce=16500290252718413717 for account: 7955621360090688183 file: 7955621360090688183_16500290252718386272_30512
+//03:04:21.798 SIG: 17198179 <= 1db3915f3b7516888973044bdce61e5489f96e8f86295440641dbf793e40b01b
+//03:04:21.798 SCP: 17198179 <= 025c7331c6b02de43d7529578de8e4b6b0937da4fe0980db880cf56b623b0bcf 6966a943cffc222ee573f20c9f63e0fe92afbaaabd0157fd3adce535d3000d99
+//03:04:22.085 SSEF: found deadline=2182819 scoop=2196 nonce=16737781616095620310 for account: 7955621360090688183 file: 7955621360090688183_16737781616095618816_3808
+//03:04:22.085 SIG: 2182819 <= 1db3915f3b7516888973044bdce61e5489f96e8f86295440641dbf793e40b01b
+//03:04:22.085 SCP: 2182819 <= 14afb50603110dce86880fd58a5934244d54551fecea9133fb9396d6bb72fa07 9af19a6ce0a88ca6ed82c209c31a0e2282184e60b608f75f77ac94f4d5fbdc34
+//03:04:22.747 SSEF: found deadline=1746506 scoop=2196 nonce=17165330504970567341 for account: 7955621360090688183 file: 7955621360090688183_17165330504970300192_3906248
+//03:04:22.747 SIG: 1746506 <= 1db3915f3b7516888973044bdce61e5489f96e8f86295440641dbf793e40b01b
+//03:04:22.747 SCP: 1746506 <= d5a481aa61c8f488de83f377a86850d26df089b6830b1fcb3d6e44361c7792da 9535c4157dd14fb240ae4a3b8b7fc64676bb91e92c19dadd5a4f732a6abe4e4c
+//03:04:33.711 SSEF: found deadline=372025 scoop=2196 nonce=16500290252708090652 for account: 7955621360090688183 file: 7955621360090688183_16500290252708073168_30512
+//03:04:33.711 SIG: 372025 <= 1db3915f3b7516888973044bdce61e5489f96e8f86295440641dbf793e40b01b
+//03:04:33.711 SCP: 372025 <= 351b62f57022093cd9e1c0ee234f48bc29f1a6d48dfd0273c6bc596143b8d586 5aa721105c7995218cffcdaacb06265c3109dc12086949554f5bc0f310516949
+//03:04:33.938 [ 7955621360090688183|Boom      |Sender] DL confirmed :      372025       4d 07:20:25
+//03:06:07.719 SSEF: found deadline=20056547 scoop=2196 nonce=16737781616097994948 for account: 7955621360090688183 file: 7955621360090688183_16737781616097979640_19072
+//03:06:07.720 SIG: 20056547 <= 1db3915f3b7516888973044bdce61e5489f96e8f86295440641dbf793e40b01b
+//03:06:07.720 SCP: 20056547 <= 6db94c6e53b022203deca8b638a0ce41579c9358682c4db32151a299bfe0db77 d2bf7218881d8d7ff5ab579c5d767f0d00de14d343479b669f7491861b1a98aa
+//03:06:11.966 SSEF: found deadline=19818361 scoop=2196 nonce=16500290252713036834 for account: 7955621360090688183 file: 7955621360090688183_16500290252713016160_30512
+//03:06:11.966 SIG: 19818361 <= 1db3915f3b7516888973044bdce61e5489f96e8f86295440641dbf793e40b01b
+//03:06:11.966 SCP: 19818361 <= a5f481be99f4d58046d73f6d83bb21561f997689cae83a3e3a1307b4d13101bf 99d84428f1a4cf3f4b280791c73179337955af5d39ce5a31a77193b69cc2a60b
+//03:06:12.089 SSEF: found deadline=17198179 scoop=2196 nonce=16500290252718413717 for account: 7955621360090688183 file: 7955621360090688183_16500290252718386272_30512
+//03:06:12.089 SIG: 17198179 <= 1db3915f3b7516888973044bdce61e5489f96e8f86295440641dbf793e40b01b
+//03:06:12.089 SCP: 17198179 <= 025c7331c6b02de43d7529578de8e4b6b0937da4fe0980db880cf56b623b0bcf 6966a943cffc222ee573f20c9f63e0fe92afbaaabd0157fd3adce535d3000d99
+//03:06:12.265 SSEF: found deadline=2182819 scoop=2196 nonce=16737781616095620310 for account: 7955621360090688183 file: 7955621360090688183_16737781616095618816_3808
+//03:06:12.265 SIG: 2182819 <= 1db3915f3b7516888973044bdce61e5489f96e8f86295440641dbf793e40b01b
+//03:06:12.265 SCP: 2182819 <= 14afb50603110dce86880fd58a5934244d54551fecea9133fb9396d6bb72fa07 9af19a6ce0a88ca6ed82c209c31a0e2282184e60b608f75f77ac94f4d5fbdc34
+//03:06:13.028 SSEF: found deadline=1746506 scoop=2196 nonce=17165330504970567341 for account: 7955621360090688183 file: 7955621360090688183_17165330504970300192_3906248
+//03:06:13.028 SIG: 1746506 <= 1db3915f3b7516888973044bdce61e5489f96e8f86295440641dbf793e40b01b
+//03:06:13.028 SCP: 1746506 <= d5a481aa61c8f488de83f377a86850d26df089b6830b1fcb3d6e44361c7792da 9535c4157dd14fb240ae4a3b8b7fc64676bb91e92c19dadd5a4f732a6abe4e4c
+//03:06:24.006 SSEF: found deadline=372025 scoop=2196 nonce=16500290252708090652 for account: 7955621360090688183 file: 7955621360090688183_16500290252708073168_30512
+//03:06:24.006 SIG: 372025 <= 1db3915f3b7516888973044bdce61e5489f96e8f86295440641dbf793e40b01b
+//03:06:24.006 SCP: 372025 <= 351b62f57022093cd9e1c0ee234f48bc29f1a6d48dfd0273c6bc596143b8d586 5aa721105c7995218cffcdaacb06265c3109dc12086949554f5bc0f310516949
+//03:06:24.250 Boom: {"error":{"message":"DL verification failed, your plot file might be corrupt. AccountId: 7955621360090688183 | Nonce: 16500290252708090652","code":10}}
+//^^
+
+//03:30:59.429 Confirmer Burst: Deadline 7353174 sent with error: {"errorCode":"1008","errorDescription":"deadline exceeds deadline limit of the pool"}
+//03:30:59.430 Confirmer Burst: Deadline should have been accepted (7353174 <= 13080869). Fast block or corrupted file?
+//03:30:58.749 SSEF: found deadline=7353174 scoop=3703 nonce=16500290252700187226 for account: 7955621360090688183 file: 7955621360090688183_16500290252700170560_30512
+//03:30:58.749 SIG: 7353174 <= 9134343e90f43d05223977cf162f822790c377493cd19a5f4aa471e539e9dd04
+//03:30:58.749 SCP: 7353174 <= e78ff8bf5fb5cfdbba7df5906a298697d14093f6a2e41a48229bf3b9a2f171d9 2c3240468b78f7c2ba4900293735c3fcd89d18615513e776fd4ad93b87d98bfd
+//03:30:59.033 [ 7955621360090688183|Burst     |Sender] DL sent      :     7353174      85d 02:32:54
+//03:30:59.033 [ 7955621360090688183] Sender Burst: Setting bests targetDL:    7353174
+//03:30:59.430 [ERROR 1008] Burst: deadline exceeds deadline limit of the pool
+//03:31:14.403 New block, no mining has been interrupted.
+//03:31:14.403 Coin queue: Burst (637290).
+//03:31:14.403 New round for Burst.
+//03:31:14.403 Updating mining information for Burst.
+//03:31:14.403 Calculating scoop for Burst
+//03:31:14.403 Resetting directories for Burst.
+//03:31:14.403 ------------------------    New Burst block: 637290
+//03:30:48.874 *! GMI Burst: Received new mining info: HTTP/1.0 200 OK\r\nX-Ratelimit-Limit: 3\r\nX-Ratelimit-Remaining: 2\r\nX-Ratelimit-Reset: 1\r\nDate: Sun, 30 Jun 2019 01:30:50 GMT\r\nContent-Length: 151\r\nContent-Type: text/plain; charset=utf-8\r\n\r\n{"baseTarget":43762,"generationSignature":"a3e990207006d703338caf0cda280f182a65ca6b6819c88db590a64a8e1bf22d","height":637290,"targetDeadline":31536000}
+//03:30:48.874 *! GMI Burst: newTargetDeadlineInfo: 31536000
+//03:30:48.874 *! GMI Burst: my_target_deadline: 13080869
+//03:30:48.874 *! GMI Burst: Using target deadline from configuration: 13080869
+//03:30:48.878 Signature for Burst changed.
+//03:30:48.878 Inserting Burst into queue.
+//03:30:48.878 Adding Burst to the end of the queue.
+//03:30:48.878 Interrupting current mining progress. New Burst block.
+//03:30:48.878 Interrupting worker threads.
+
+//03:38:32.175 Boom: {"error":{"message":"DL verification failed, your plot file might be corrupt. AccountId: 7955621360090688183 | Nonce: 16500290252697440744","code":10}}
+//03:35:01.499 SSEF: found deadline=1581934 scoop=1824 nonce=16500290252697440744 for account: 7955621360090688183 file: 7955621360090688183_16500290252697424480_30512
+//03:35:01.499 SIG: 1581934 <= a2563a7e448d85ee68cb5d93357c525e858cbe3bac00ed41b92d90716f98db50
+//03:35:01.499 SCP: 1581934 <= 89ffbf685572d1dedf01e332f19560fdb0fcfdef6a6680e321fe16f165b42c00 eb4aa9af84869353b64fadf05f7f62da73e684810403192e1c20108cf47d8d5d
+//03:35:01.760 [ 7955621360090688183|Boom      |Sender] DL confirmed :     1581934      18d 07:25:34
+//03:38:31.894 SSEF: found deadline=1581934 scoop=1824 nonce=16500290252697440744 for account: 7955621360090688183 file: 7955621360090688183_16500290252697424480_30512
+//03:38:31.894 SIG: 1581934 <= a2563a7e448d85ee68cb5d93357c525e858cbe3bac00ed41b92d90716f98db50
+//03:38:31.894 SCP: 1581934 <= 89ffbf685572d1dedf01e332f19560fdb0fcfdef6a6680e321fe16f165b42c00 eb4aa9af84869353b64fadf05f7f62da73e684810403192e1c20108cf47d8d5d
+//^^
+
+//03:38:50.521 Boom: {"error":{"message":"DL verification failed, your plot file might be corrupt. AccountId: 7955621360090688183 | Nonce: 13119679609471665935","code":10}}
+//03:35:20.486 SSEF: found deadline=804093 scoop=1824 nonce=13119679609471665935 for account: 7955621360090688183 file: 7955621360090688183_13119679609471646960_30512
+//03:35:20.486 SIG: 804093 <= a2563a7e448d85ee68cb5d93357c525e858cbe3bac00ed41b92d90716f98db50
+//03:35:20.486 SCP: 804093 <= 8ac79a40a78400462f40d01b47b0d735215da62e8709ebc0881a678562492724 a78d6262f36506bcf0f1704f7a5ad5faa2ee65910286ae5981a61fe1d579f934
+//03:35:20.723 [ 7955621360090688183|Boom      |Sender] DL confirmed :      804093       9d 07:21:33
+//03:38:50.258 SSEF: found deadline=804093 scoop=1824 nonce=13119679609471665935 for account: 7955621360090688183 file: 7955621360090688183_13119679609471646960_30512
+//03:38:50.258 SIG: 804093 <= a2563a7e448d85ee68cb5d93357c525e858cbe3bac00ed41b92d90716f98db50
+//03:38:50.258 SCP: 804093 <= 8ac79a40a78400462f40d01b47b0d735215da62e8709ebc0881a678562492724 a78d6262f36506bcf0f1704f7a5ad5faa2ee65910286ae5981a61fe1d579f934
+
+//04:00:31.037 BHD: {"error":{"message":"Submission is for a different round","code":9}}
+//04:00:30.727 SSEF: found deadline=512325 scoop=1111 nonce=17165330504961285034 for account: 7955621360090688183 file: 7955621360090688183_17165330504958581448_3906248
+//04:00:30.727 SIG: 512325 <= baf9649af661f7a1b2ffcb769ea54c4eba97cd7364224b2d42832b500e8797c8
+//04:00:30.727 SCP: 512325 <= 21ed1fbf4dc109064579f86b8ca0c60bda1889ae90cad9d6fc50778bd5dd8cd1 3b96bf0b297b785fc9073f3ca33c621bb4753c6a6488e6b68713f21ddbf630f4
+//04:00:30.727 [ 7955621360090688183|BHD       |Worker] DL found     :      512325
+//03:59:41.192 *! GMI BHD: Received new mining info: HTTP/1.1 200 OK\r\nContent-Type: application/json; charset=utf-8\r\nContent-Length: 150\r\nDate: Sun, 30 Jun 2019 01:59:41 GMT\r\nConnection: close\r\n\r\n{"height":180840,"baseTarget":9929,"generationSignature":"baf9649af661f7a1b2ffcb769ea54c4eba97cd7364224b2d42832b500e8797c8","targetDeadline":31536000}
+
+//04:46:32.925 Confirmer BHD: ! Error getting confirmation for DL: 29974075  code: 10060
+//^ Socket Error # 10060 Connection timed out -- and the codebase is NEW, it already shows SSEF/SIG/SCP, why still 10060 shows up?!
+//^ btw, the submission was auto retried few seconds later and succeeded
+//04:46:31.399 SSEF: found deadline=29974075 scoop=3885 nonce=16737781616096137891 for account: 7955621360090688183 file: 7955621360090688183_16737781616096125856_15256
+//04:46:31.399 SIG: 29974075 <= 74cc60cfd4a3913a1dd01e6d63614fc5823236b6c6829186902fb8a1f4e3eb65
+//04:46:31.399 SCP: 29974075 <= edcb8be7f002b6b897b617d6a9d2e3b2da70b22fd80fc6da0c893507ad827747 917ffd0b8d7e31b58790ed76dae667741607057c1d80455e4f7d1c295083867f
+
+
+// FYI: a few cases of CRASHES was not diagnosed. I have no idea why it happened, but it's important to note that both crashes occurred
+// while mining BOOM and it was just after receiving response from the server
+
+
+// FYI: MANY of the 'corrupted' errors come from failed thread interrupting. new GMI arrived, waits for interruption, and the worker threads happily continue
+// when the worker threads note the new block (sometimes even 10-30s later!) in the meantime another GMI could show up and invalidate the previous already..
+// happens on BOOM often
+// also might happen in conjunction with coin switch: mining boom at 100, switch to BHD, in the mean time GMI Boom comes with 101, BHD ends, Boom continues with 100
+// also some submissions are reported as wrong due to network lag and GMI:
+// Boom mining at 100, found DL, queued for submitting, new GMI arrived, Boom now at 101, confirmer picks up old DL from 100 and tries to send it -> failure
+// Same happens when the confirmer managed to SEND before GMI arrived, but is waiting for confirmation and GMI arrives in the meanwhile - error is reported, even though it's irrelevant error
+// actually there is no SEND queue.. only REVC queue, so above 2 cases are the same 1 case
+
+
+// FYI: one strange case for rejection was when Boom 776 block was ROLLED BACK
+/*
+10:00:06.562 *! GMI Boom: Received new mining info: {"height":774,"baseTarget":85193,"generationSignature":"10f12fad21e207191cd7d3431c6a8ab0ac3f5fe0619b9c870aa7f1fbb081daad","targetDeadline":31536000}
+...
+10:02:26.049 *! GMI Boom: Received new mining info: {"height":775,"baseTarget":90198,"generationSignature":"8d7cd32e09ad431b1cc5c5eb18d469db48ea32f4d12add3b6ffc018e3887c5db","targetDeadline":31536000}
+...
+10:12:54.348 *! GMI Boom: Received new mining info: {"height":776,"baseTarget":93571,"generationSignature":"459df435747c2bec7a8b2cedc5451459e6299d45faa3696deaec1ec43e6e6e47","targetDeadline":31536000}
+...
+10:15:22.572 *! GMI Boom: Received new mining info: {"height":775,"baseTarget":90198,"generationSignature":"8d7cd32e09ad431b1cc5c5eb18d469db48ea32f4d12add3b6ffc018e3887c5db","targetDeadline":31536000}
+...
+10:15:23.140 *! GMI Boom: Received new mining info: {"height":776,"baseTarget":93571,"generationSignature":"e852beacb404074ca6cab276489cb17858ff7a5ba63842b497106a0906d4ce99","targetDeadline":31536000}
+...
+10:15:27.703 *! GMI Boom: Received new mining info: {"height":777,"baseTarget":97131,"generationSignature":"9fe844efa15f37c13e2f76b8a566935a966054156ce05d5dcba625ff0c8eca32","targetDeadline":31536000}
+*/
+// gensig of 775 did not change, gensig of 776 did change
+// the miner POLLS the GMI. It first caught original 776 gensig, then it was rolled back, the miner polled again and got 775 (776 was missing for short time),
+// the miner treated it as block-height-change and processed it even though EARLIER it already processed and finished block 775
+// the miner was re/processing 775 and new GMI with new 776 arrived, but the miner failed interrupting threads and LOTS OF TIME was wasted before processing 776 has been started
